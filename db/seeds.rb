d06626adcc43d5adf71c51a30df5be120790ff0b
otavio gomes <otaviojdggomes@gmail.com>
2023-10-14 10:37:11 -0300

    simple airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# db/seeds.rb
+
+# Flat 1
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen, and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+# Flat 2
+Flat.create!(
+  name: 'Cozy Apartment in the Heart of Paris',
+  address: '123 Rue de la Paix, 75000 Paris, France',
+  description: 'Charming apartment in a historic building with a view of the Eiffel Tower. Perfect for a romantic getaway.',
+  price_per_night: 120,
+  number_of_guests: 2
+)
+
+# Flat 3
+Flat.create!(
+  name: 'Sunny Beachfront Villa in Malibu',
+  address: '123 Ocean Drive, Malibu, CA, USA',
+  description: 'Stunning beachfront villa with panoramic ocean views. Private pool and direct beach access.',
+  price_per_night: 350,
+  number_of_guests: 6
+)
+
+# Flat 4
+Flat.create!(
+  name: 'Mountain Retreat in Aspen',
+  address: '456 Mountain Road, Aspen, CO, USA',
+  description: 'Rustic cabin in the mountains with a cozy fireplace and scenic views. Great for a ski vacation.',
+  price_per_night: 200,
+  number_of_guests: 4
+)
+
